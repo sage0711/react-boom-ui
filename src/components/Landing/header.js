ba86ace79faa2f2641171cd4ffedb41d1ba9a29b
redlion2375 <wintermountain2375@gmail.com>
2023-06-15 15:42:20 +0900

    final commit

--- src/components/Landing/header.js
+++ src/components/Landing/header.js
@@ -7,15 +7,17 @@ const Header = () => {
 
     return (
         <div className='landing-header'>
-            <div className="logo-container">
-                <img src={logo} className='logo' />
-            </div>
-            <div className="btn-grp">
-                <span className='link-item'><a href="#">PRESALE</a></span>
-                <span className='link-item'><a href="#">INFO</a></span>
-                <span className='link-item'><a href="#">TOKENOMICS</a></span>
-                <img src={twitter} className="twitter" />
-                <img src={telegram} className="telegram" />
+            <div className="pack">
+                <div className="logo-container">
+                    <img src={logo} className='logo' />
+                </div>
+                <div className="btn-grp">
+                    <span className='link-item'><a href="#">PRESALE</a></span>
+                    <span className='link-item'><a href="#">INFO</a></span>
+                    <span className='link-item'><a href="#">TOKENOMICS</a></span>
+                    <img src={twitter} className="twitter" />
+                    <img src={telegram} className="telegram" />
+                </div>
             </div>
         </div>
     );
